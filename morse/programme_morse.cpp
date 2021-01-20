cc4e3419e485aeee0a71fbb8a620928eddf97ffa
Adrien <adrien.mignot@mines-paristech.fr>
2021-01-20 20:28:32 +0100

    premier ajout de morse

---
+++ morse/programme_morse.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include "dictionnaire_et_fonctions.cpp"
+
+std::string traduction() {
+    std::cout << "  1) Traduire un message du français en un texte morse" << std::endl;
+    std::cout << "  2) Convertir un fichier texte du français en un texte morse" << std::endl;
+    std::cout << "  3) Décoder un texte morse en un fichier texte en français" << std::endl;
+    int souhait;
+    std::cin >> souhait;
+   
+   if (souhait == 1){
+       std::cout << "tapez le texte que vous souhaitez voir converti en un fichier audio morse" << std::endl;
+       std::string texte_entree;
+       std::cin >> texte_entree;
+       std::string texte_sortie = traduction_message_francais_vers_morse(texte_entree);
+       return texte_sortie;}
+
+    else{return "1";};
+   }
